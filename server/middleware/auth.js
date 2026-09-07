1e25dea7d5ab8e99d7f9eb24759722fe2738b08d
tabbiotech05-beep <tabbiotech05@gmail.com>
2026-09-07 01:27:56 +0100

    Feat: Support token in query string for file downloads

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
 
 const auth = async (req, res, next) => {
-    const token = req.header('x-auth-token');
+    const token = req.header('x-auth-token') || req.query.token;
 
     if (!token) {
         return res.status(401).json({ msg: 'No token, authorization denied' });
